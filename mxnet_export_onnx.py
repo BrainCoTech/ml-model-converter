292ecd0b0a86952b7aab74a3d2813a807399ffa4
Disi A <adis@live.cn>
2020-12-14 16:58:58 +0800

    Update mxnet export python scripts.

--- mxnet_export_onnx.py
+++ mxnet_export_onnx.py
@@ -6,6 +6,15 @@ import mxnet as mx
 import numpy as np
 from mxnet.contrib import onnx as onnx_mxnet
 
+from enum import IntEnum  # Enum declarations
+
+class Product(IntEnum):
+    crimson = 0
+    stark = 1
+    emg_plus = 2
+
+product = Product.Crimson
+
 print(sys.argv[0])
 if len(sys.argv) != 4:
     print("USAGE:python mxnet_exort_onnx.py [MODEL_FILE] [PARAMS_file] [OUTPUT_FILE]")
@@ -17,8 +26,16 @@ symbol_file = sys.argv[1]
 params_file = sys.argv[2]
 output_file = sys.argv[3]
 
+input_shape = None
+if product == Product.crimson:
+    input_shape = (1,1,1250)
+else if product == Product.stark:
+    input_shape = (1,1,100,12)
+else if product == Product.emg_plug
+    input_shape = (1,1,100,12)
+
 # https://mxnet.cdn.apache.org/versions/1.7.0/api/python/docs/tutorials/deploy/export/onnx.htmlhttps://mxnet.cdn.apache.org/versions/1.7.0/api/python/docs/tutorials/deploy/export/onnx.html
-onnx_mxnet.export_model(symbol_file, params_file, [(1,1,1250)], input_type=np.float32, onnx_file_path=output_file, verbose=False)
+onnx_mxnet.export_model(symbol_file, params_file, [input_shape], input_type=np.float32, onnx_file_path=output_file, verbose=False)
 
 # Load the model along with the transformations
 #net = mx.gluon.SymbolBlock.imports(symbol_file=symbol_file, input_names=['data'], param_file=params_file, ctx=mx.cpu())
