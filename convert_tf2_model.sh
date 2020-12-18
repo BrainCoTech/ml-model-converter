85affcae07ee2e0d85ec555c17318d50c97c1989
Disi A <adis@live.cn>
2020-12-19 01:25:23 +0800

    Bug fix.

--- convert_tf2_model.sh
+++ convert_tf2_model.sh
@@ -7,7 +7,7 @@ fi
 MODEL_PATH=$1
 OUTPUT_FILE_NAME=$2
 pip3 install tf2onnx
-python3 -m tf2onnx.convert --saved-model ${MODEL_NAME} --output tmp.onnx
+python3 -m tf2onnx.convert --saved-model ${MODEL_PATH} --output tmp.onnx
 
 ./convert_onnx_model.sh tmp.onnx ${OUTPUT_FILE_NAME}
 rm tmp.onnx
